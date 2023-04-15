33f7436d82142d8b489ee9ab693a8fe91302fa18
Liu Zihua <lukeliu2020@126.com>
2023-04-15 14:33:53 +0900

    add

--- models/DSSMD.py
+++ models/DSSMD.py
@@ -43,7 +43,11 @@ class DSSMMD(nn.Module):
         self.iconv0_sm = nn.ConvTranspose2d(19,16, 3, 1, 1)
         
         
-        self.upconv5_sm = deconv(512, 256)
+        if self.dehaze_switch:
+            self.upconv5_sm = deconv(512+512, 256)
+        else:
+            self.upconv5_sm = deconv(512, 256)
+        
         self.upconv4_sm = deconv(256, 128)
         self.upconv3_sm = deconv(128, 64)
         self.upconv2_sm = deconv(64, 32)
@@ -90,7 +94,7 @@ class DSSMMD(nn.Module):
             self.iconv0_trans = nn.ConvTranspose2d(19,16, 3, 1, 1)
             
             
-            self.upconv5_trans = deconv(512, 256)
+            self.upconv5_trans = deconv(512+512, 256)
             self.upconv4_trans = deconv(256, 128)
             self.upconv3_trans = deconv(128, 64)
             self.upconv2_trans = deconv(64, 32)
@@ -147,8 +151,33 @@ class DSSMMD(nn.Module):
         conv6b_sm = self.conv6_1_sm(conv6a_sm)       # 512 1/64
         
         
-        # Upsample
-        upconv5_sm = self.upconv5_sm(conv6b_sm)      # 256 1/32        
+        # The dehazing branch encoder.
+        if self.dehaze_switch:
+            # downsample 1
+            conv1_l_trans = self.conv1_trans(haze_left)          # 32 1/2
+            conv2_l_trans = self.conv2_trans(conv1_l_trans)           # 64 1/4
+            conv3_l_trans = self.conv3_sm(conv2_l_trans)           # 128 1/8
+            
+            
+            # Further Downsample
+            conv4a_trans = self.conv4_trans(conv3_l_trans)         
+            conv4b_trans = self.conv4_1_trans(conv4a_trans)       # 256 1/16
+            conv5a_trans = self.conv5_trans(conv4b_trans)
+            conv5b_trans = self.conv5_1_trans(conv5a_trans)       # 256 1/32
+            conv6a_trans = self.conv6_trans(conv5b_trans)
+            conv6b_trans = self.conv6_1_trans(conv6a_trans)       # 512 1/64
+            
+
+            
+
+
+
+        # The stereo matching branch decoder.
+        if self.dehaze_switch:
+            transmision_stereo_fusion = torch.cat((conv6b_sm,conv6b_trans),dim=1)
+            upconv5_sm = self.upconv5_sm(transmision_stereo_fusion)      # 256 1/32
+        else:
+            upconv5_sm = self.upconv5_sm(conv6b_sm)      # 256 1/32        
         concat5_sm = torch.cat((upconv5_sm,conv5b_sm),dim=1)
         iconv5_sm = self.iconv5_sm(concat5_sm)       # 256
         
@@ -157,7 +186,6 @@ class DSSMMD(nn.Module):
         concat4_sm = torch.cat((upconv4_sm, conv4b_sm), dim=1)   # 384 1/16
         iconv4_sm = self.iconv4_sm(concat4_sm)       # 256 1/16
         
-        
         upconv3_sm = self.upconv3_sm(iconv4_sm)      # 64 1/8
         concat3_sm = torch.cat((upconv3_sm, conv3b_sm), dim=1)    # 64+128=192 1/8
         iconv3_sm = self.iconv3_sm(concat3_sm)       # 64
@@ -165,8 +193,6 @@ class DSSMMD(nn.Module):
         # 1/8 Disparity Estimation.
         disp3 = self.disp3(iconv3_sm)
         disp3 = self.relu3(disp3)
-        
-        
         
         # 1/4 disparity estimation
         upconv2_sm = self.upconv2_sm(iconv3_sm)      # 32 1/4
@@ -197,22 +223,13 @@ class DSSMMD(nn.Module):
         disp0 = disp0 + res0
         disp0 = self.relu0(disp0)
         
+        
+        
+        # dehazing branch decoder.
+
         if self.dehaze_switch:
-            # downsample 1
-            conv1_l_trans = self.conv1_trans(haze_left)          # 32 1/2
-            conv2_l_trans = self.conv2_trans(conv1_l_trans)           # 64 1/4
-            conv3_l_trans = self.conv3_sm(conv2_l_trans)           # 128 1/8
-            
-            
-            # Further Downsample
-            conv4a_trans = self.conv4_trans(conv3_l_trans)         
-            conv4b_trans = self.conv4_1_trans(conv4a_trans)       # 256 1/16
-            conv5a_trans = self.conv5_trans(conv4b_trans)
-            conv5b_trans = self.conv5_1_trans(conv5a_trans)       # 256 1/32
-            conv6a_trans = self.conv6_trans(conv5b_trans)
-            conv6b_trans = self.conv6_1_trans(conv6a_trans)       # 512 1/64
-            
-            upconv5_trans = self.upconv5_trans(conv6b_trans)      # 256 1/32        
+        
+            upconv5_trans = self.upconv5_trans(transmision_stereo_fusion)      # 256 1/32        
             concat5_trans = torch.cat((upconv5_trans,conv5b_trans),dim=1)
             iconv5_trans = self.iconv5_trans(concat5_trans)       # 256
 
@@ -242,12 +259,6 @@ class DSSMMD(nn.Module):
             predicted_transmission = self.transmision_estimation_branch(iconv0_trans)
             
             
-            # Airlight Estimation
-            # self.conv1_air = conv(3,32,7,2) # 1/2
-            # self.conv2_air = conv(32,48,kernel_size=3,stride=2,batchNorm=True) # 1/4
-            # self.conv3_air = conv(48,32,kernel_size=3,stride=2,batchNorm=True) # 1/8
-            # self.conv4_air = conv(32,16,kernel_size=3,stride=1,batchNorm=True) # 1/16
-            # self.conv5_air = conv(16,1,kernel_size=3,stride=1,batchNorm=True)
             
             conv1_air = self.conv1_air(haze_left)
             conv2_air = self.conv2_air(conv1_air)
@@ -256,13 +267,13 @@ class DSSMMD(nn.Module):
             conv5_air = self.conv5_air(conv4_air)
 
             airlight = F.max_pool2d(conv5_air, kernel_size=conv5_air.size()[2:]).squeeze(-1).squeeze(-1) #[B,1]
-            
+        
+
             return [disp3,disp2,disp1,disp0],predicted_transmission,airlight
         
         
         else:
             return [disp3,disp2,disp1,disp0]
-        
 
         
              
